a392b0d3ce9fdae3760b8b5b5e0d6b56a4f42e07
pedro <pedroigor91@gmail.com>
2017-09-27 18:01:41 -0300

    Criando o componente de Posts.

--- Blog/src/reducers/index.js
+++ Blog/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
+import Postreducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: Postreducer,
+  form: formReducer
 });
 
 export default rootReducer;
