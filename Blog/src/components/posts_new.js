ef090b860ac40cb3765370abbe8588afe3158d0d
pedro <pedroigor91@gmail.com>
2017-09-27 23:44:59 -0300

    Implementando o submit do form. Implementado a mudança de estilo de acordo com a condição. Implementado o botão de cancelar.

--- Blog/src/components/posts_new.js
+++ Blog/src/components/posts_new.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input className="form-control" type="text" {...field.input} />
+
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field label="Title for Post" name="title" component={this.renderField} />
                 <Field label="Categories" name="categories" component={this.renderField} />
                 <Field label="Post Content" name="content" component={this.renderField} />
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -25,9 +42,9 @@ class PostsNew extends Component {
 function validate(values) {
     const errors = {};
 
-    if (values.title.length < 3) {
+    /*if (values.title.length < 3) {
         errors.title = 'Title must be at least 3 characters!';
-    }
+    }*/
 
     if (!values.title) {
         errors.title = "Enter a title";
